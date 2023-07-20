2c0caf979fde8a86aa880b9f229322d70c8d4463
tr_st <johndoe@example.com>
2023-07-20 21:33:55 +0300

    Final

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.get_record, name='record'),
+    path('record/update/<int:pk>', views.update_record, name='update_record'),
+    path('record/delete/<int:pk>', views.delete_record, name='delete_record'),
+    path('record/add/', views.add_record, name='add_record'),
 ]
