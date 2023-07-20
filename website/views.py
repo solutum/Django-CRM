aab2d8efcc6a7ee171437af3f9a90203293ea9e1
tr_st <johndoe@example.com>
2023-07-20 15:41:13 +0300

    Authentication

--- website/views.py
+++ website/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages # show messages to user
+
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {
+	# check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Auth
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You have been logged in!")
+		else:
+			messages.success(request, "There was an error")
+		return redirect('home')
 
+	return render(request, 'home.html', {
 	})
+
+#def login_user(request):
+#	pass
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You have been Logged Out")
+	return redirect('home')
